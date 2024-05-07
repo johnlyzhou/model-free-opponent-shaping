fb4e236678939fc2d7ea5057825395b5629b9932
John Zhou <john.ly.zhou@gmail.com>
2024-05-07 14:53:12 -0700

    logs

--- src/main_mfos_ppo.py
+++ src/main_mfos_ppo.py
@@ -4,6 +4,7 @@ from environments import MetaGames
 import os
 import argparse
 import json
+from tqdm import tqdm
 
 
 parser = argparse.ArgumentParser()
@@ -35,19 +36,22 @@ if __name__ == "__main__":
     name = args.exp_name
 
     print(f"RUNNING NAME: {name}")
-    if not os.path.isdir(name):
-        os.mkdir(name)
-        with open(os.path.join(name, "commandline_args.txt"), "w") as f:
-            json.dump(args.__dict__, f, indent=2)
+    # if not os.path.isdir(name):
+    #     os.mkdir(name)
+    #     with open(os.path.join(name, "commandline_args.txt"), "w") as f:
+    #         json.dump(args.__dict__, f, indent=2)
 
     #############################################
 
     # creating environment
     env = MetaGames(batch_size, opponent=args.opponent, game=args.game, mmapg_id=args.mamaml_id)
 
-    action_dim = env.d
-    state_dim = env.d * 2
+    action_dim = env.d  # 5 for IPD
+    state_dim = env.d * 2  # concatenation of both policies so 10
+    print(f"action_dim: {action_dim}, state_dim: {state_dim}")
 
+    # This is actually the outer MFOS agent that is outputting a 5-d policy to play against the inner PPO agent
+    #  at each step, i.e., a full inner episode
     memory = Memory()
     ppo = PPO(state_dim, action_dim, lr, betas, gamma, K_epochs, eps_clip, args.entropy)
 
@@ -63,25 +67,30 @@ if __name__ == "__main__":
 
     # training loop
     rew_means = []
+    logs = []
 
-    for i_episode in range(1, max_episodes + 1):
+    for i_episode in tqdm(range(1, max_episodes + 1)):
         state = env.reset()
 
-        running_reward = torch.zeros(batch_size).cuda()
-        running_opp_reward = torch.zeros(batch_size).cuda()
+        running_reward = torch.zeros(batch_size)  #.cuda()
+        running_opp_reward = torch.zeros(batch_size)  #.cuda()
 
         last_reward = 0
+        last_opp_reward = 0
 
         for t in range(num_steps):
 
             # Running policy_old:
             action = ppo.policy_old.act(state, memory)
+            # Env step gives the full rewards of an entire inner episode between the two agents
             state, reward, info, M = env.step(action)
 
             memory.rewards.append(reward)
             running_reward += reward.squeeze(-1)
             running_opp_reward += info.squeeze(-1)
-            last_reward = reward.squeeze(-1)
+            last_reward = reward.squeeze(-1).mean()
+            last_opp_reward = info.squeeze(-1).mean()
+            # print(last_reward.shape, last_opp_reward.shape)
 
         ppo.update(memory)
         memory.clear_memory()
@@ -99,15 +108,23 @@ if __name__ == "__main__":
             }
         )
 
-        print(f"opponent loss: {-running_opp_reward.mean() / num_steps}", flush=True)
+        logs.append({f"player_{0}": {"last_reward": last_reward},
+                     f"player_{1}": {"last_reward": last_opp_reward}})
+        print(logs[-1])
 
         if i_episode % save_freq == 0:
-            ppo.save(os.path.join(name, f"{i_episode}.pth"))
             with open(os.path.join(name, f"out_{i_episode}.json"), "w") as f:
-                json.dump(rew_means, f)
-            print(f"SAVING! {i_episode}")
+                json.dump(logs, f)
 
-    ppo.save(os.path.join(name, f"{i_episode}.pth"))
-    with open(os.path.join(name, f"out_{i_episode}.json"), "w") as f:
-        json.dump(rew_means, f)
-    print(f"SAVING! {i_episode}")
+    #     print(f"opponent loss: {-running_opp_reward.mean() / num_steps}", flush=True)
+    #
+    #     if i_episode % save_freq == 0:
+    #         ppo.save(os.path.join(name, f"{i_episode}.pth"))
+    #         with open(os.path.join(name, f"out_{i_episode}.json"), "w") as f:
+    #             json.dump(rew_means, f)
+    #         print(f"SAVING! {i_episode}")
+    #
+    # ppo.save(os.path.join(name, f"{i_episode}.pth"))
+    # with open(os.path.join(name, f"out_{i_episode}.json"), "w") as f:
+    #     json.dump(rew_means, f)
+    # print(f"SAVING! {i_episode}")
