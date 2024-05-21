e4edf9caef71cd581fa393793db62ba0bc3dc21e
John Zhou <john.ly.zhou@gmail.com>
2024-05-21 11:11:12 -0700

    parallelize

--- run_ipd_mamaml.py
+++ run_ipd_mamaml.py
@@ -1,10 +1,14 @@
 import argparse
 import json
 import os
+from time import sleep
+import multiprocessing as mp
 
 import torch
 
 from src.main_mfos_ppo import main
+
+CUDA_LIST = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())]
 
 
 if __name__ == '__main__':
@@ -21,10 +25,36 @@ if __name__ == '__main__':
         with open(os.path.join(args.exp_name, "commandline_args.txt"), "w") as f:
             json.dump(args.__dict__, f, indent=2)
 
+    if args.device == "all":
+        device_list = CUDA_LIST
+        print("Using all available devices:", device_list)
+    elif args.device is not None:
+        device_list = [torch.device(args.device)]
+        print("Using:", device_list[0])
+    else:
+        device_list = [torch.device("cpu")]
+        print("No device specified, using CPU")
+
+    processes = [None for _ in range(len(device_list))]
+
     for mamaml_id in range(10):
         device = torch.device(args.device)
-        exp_name = f"{args.exp_name}/id_{mamaml_id}"
-        if not os.path.isdir(exp_name):
-            os.mkdir(exp_name)
-        print(f"RUNNING NAME: {exp_name}")
-        main(args.game, "MAMAML", args.entropy, exp_name, args.checkpoint, mamaml_id, device)
+        save_dir = f"{args.exp_name}_id{i}"
+        if not os.path.isdir(save_dir):
+            os.mkdir(save_dir)
+
+        while True:
+            try:
+                device_idx = processes.index(None)
+                device = device_list[device_idx]
+                break
+            except ValueError:
+                for i, p in enumerate(processes):
+                    if not p.is_alive():
+                        processes[i] = None
+                sleep(1)
+
+        process_args = (args.game, "MAMAML", args.entropy, save_dir, args.checkpoint, mamaml_id, device)
+        print(f"RUNNING NAME: {save_dir}")
+        processes[device_idx] = mp.Process(target=main, args=process_args)
+        processes[device_idx].start()
